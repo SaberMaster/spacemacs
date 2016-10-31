e777cac1db5c80dac9072649a1c4192aa92b3aa9
liuyanan <liuyanan@oradt.com>
2016-10-31 10:10:07 +0800

    [code] remove some zilongshanren org config

--- layers/zilongshanren-org/packages.el
+++ layers/zilongshanren-org/packages.el
@@ -565,7 +565,6 @@ See `org-capture-templates' for more information."
               ("b" "Blog" tags-todo "BLOG")
               ("p" . "项目安排")
               ("pw" tags-todo "PROJECT+WORK+CATEGORY=\"work\"")
-              ("pl" tags-todo "PROJECT+DREAM+CATEGORY=\"zilongshanren\"")
               ("W" "Weekly Review"
                ((stuck "") ;; review stuck projects as designated by org-stuck-projects
                 (tags-todo "PROJECT") ;; review all projects (assuming you use todo keywords to designate projects)
