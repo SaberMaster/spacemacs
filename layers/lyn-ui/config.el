d3437501e49d72463844423e60c1e69b49536d40
Lyn <lyn1990828@gmail.com>
2017-08-18 14:50:48 +0800

    [modify] add org-download and fix some ispell error

---
+++ layers/lyn-ui/config.el
@@ -0,0 +1,14 @@
+;;; config.el --- lyn-org layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+
+;; Author: Lyn <lyn1990828@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; fix ispell win can't show fully
+(setq-default ispell-choices-win-default-height 3)
